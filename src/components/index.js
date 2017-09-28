935f2e4b9ae2ea492081605790cb9f0d74476445
Taylor Johnson <taylor@venturestorm.com>
2017-09-28 18:13:02 -0400

    add more styling to the index page

--- src/components/index.js
+++ src/components/index.js
@@ -3,22 +3,52 @@
 const React = require('react');
 const Link = require('react-router').Link;
 const PROJECTS = require('../projects');
-import {Row, Col} from 'react-materialize';
+const {Parallax, Card, Row, Col} = require('react-materialize');
+
+const paraStyle = {
+    maxWidth: '100%'
+};
+
+
 
 class Index extends React.Component {
   render() {
     return (
       <div>
-      <Row className='center'>
-        <h1>Taylor Johnson</h1>
-      </Row> 
-      {
-          Object.keys(PROJECTS).map(id =>
-            <Row className='center'>
-              <div key={id}><Link to={`/project/${id}`}>{PROJECTS[id].name}</Link></div>
-            </Row>
-          )
-      }
+        <Row className='center'>
+          <h1>Taylor Johnson</h1>
+        </Row>
+        <Parallax style={paraStyle} imageSrc='/dist/images/solo-surfer.jpg' />
+
+        <div className='section white'>
+          <Row>
+            <Col s={12} m={8} offset='m2'>
+              <Card className='small hoverable' 
+              title='About'
+              actions={[
+                <a href='https://www.linkedin.com/in/taylor-johnson-8921a5a1/' target='_blank'>LinkedIn</a>,
+                <a href='https://www.facebook.com/taylor.johnson.90226' target='_blank'>Facebook</a>
+              ]}>
+                <p className='center'>Taylor Johnson is a technologist, entrepreneur, surfer, and fitness enthusiast.</p>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+
+        <div>
+          <Row>
+            <h5 className='center'>Stuff I've Built</h5>
+            <Col s={12} m={8} offset='m2'>
+              {
+                Object.keys(PROJECTS).map(id =>
+                  <Row className='center'>
+                    <div key={id}><Link to={`/project/${id}`}>{PROJECTS[id].name}</Link></div>
+                  </Row>
+                )
+              }
+            </Col>
+          </Row>
+        </div>
       </div>
     )
   }
--- src/components/projectPage.js
+++ src/components/projectPage.js
@@ -3,18 +3,25 @@
 const React = require('react');
 const Link = require('react-router').Link;
 const PROJECTS = require('../projects');
+import {Parallax, Row, Col} from 'react-materialize';
 
 class ProjectPage extends React.Component {
   render() {
     let project = PROJECTS[this.props.params.id];
     return (
       <div>
-        <h2>{project.name}</h2>
-        <ul>{
-          project.stack.map( (tech, key) =>
-            <li key={key}>{tech}</li>
-          )
-        }</ul>
+        <Row className='center'>
+          <h2>{project.name}</h2>
+        </Row>
+        <Row className='center'>
+          {
+            project.stack.map( (tech, key) => 
+              <Col key={key} s={6} m={4} l={3}>
+                {tech}
+              </Col>
+            )
+          }
+        </Row>
         <Link to="/">Home</Link>
       </div>
     );
